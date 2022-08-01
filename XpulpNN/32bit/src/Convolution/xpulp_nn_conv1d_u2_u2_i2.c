79c7bb4939d73efd98dd710f9e3ffcf20eba0383
Joan Mihali <jmihali@student.ethz.ch>
2022-08-01 11:48:34 +0200

    change _y to _x in xpulpnn conv1d

--- XpulpNN/32bit/src/Convolution/xpulp_nn_conv1d_u2_u2_i2.c
+++ XpulpNN/32bit/src/Convolution/xpulp_nn_conv1d_u2_u2_i2.c
@@ -32,28 +32,28 @@ void __attribute__((noinline)) xpulp_nn_conv1d_u2_u2_i2(
                         int32_t *pLambda,
                         uint16_t out_mult,
                         uint16_t out_shift,
-                        uint16_t dim_in_y,
+                        uint16_t dim_in_x,
                         uint16_t ch_in,
-                        uint16_t dim_out_y,
+                        uint16_t dim_out_x,
                         uint16_t ch_out,
-                        uint16_t dim_kernel_y,
-                        uint16_t padding_y_top,
-                        uint16_t padding_y_bottom,
-                        uint16_t stride_y,
-                        uint16_t dilation,
+                        uint16_t dim_kernel_x,
+                        uint16_t padding_x_left,
+                        uint16_t padding_x_right,
+                        uint16_t stride_x,
+                        uint16_t dilation_x,
                         uint8_t flag_relu,
                         uint8_t flag_batch_norm)
 {
   uint16_t ch_in_r = PACK_INT2_SIZE(ch_in);
   uint16_t ch_out_r = PACK_INT2_SIZE(ch_out);
-  uint16_t dil = dilation - 1;
+  uint16_t dil = dilation_x - 1;
 
   int core_id = pi_core_id();
-  uint8_t * pIm2ColBase = pIm2ColBuffer + (2 * core_id * PACK_INT2_SIZE(ch_in) * dim_kernel_y);
-  int i_out_y, i_ker_y;
+  uint8_t * pIm2ColBase = pIm2ColBuffer + (2 * core_id * PACK_INT2_SIZE(ch_in) * dim_kernel_x);
+  int i_out_x, i_ker_x;
   int Log2Core;
 
-  uint8_t extra_chunk = ((dim_out_y & (NUM_CORES-1)) != 0);
+  uint8_t extra_chunk = ((dim_out_x & (NUM_CORES-1)) != 0);
   uint8_t extra_chunk_r;
   uint8_t section;
   int core_id_r;
@@ -64,55 +64,55 @@ void __attribute__((noinline)) xpulp_nn_conv1d_u2_u2_i2(
   extra_chunk_r = extra_chunk;
   extra_chunk = 0;
 
-  int chunk = (dim_out_y >> Log2Core) + extra_chunk_r;
+  int chunk = (dim_out_x >> Log2Core) + extra_chunk_r;
 
-  int start_pixel = min((chunk * core_id_r), dim_out_y);
-  int stop_pixel = min(start_pixel + chunk, dim_out_y);
+  int start_pixel = min((chunk * core_id_r), dim_out_x);
+  int stop_pixel = min(start_pixel + chunk, dim_out_x);
 
   uint8_t *pIm2Col = pIm2ColBase;
   uint8_t *pOutBuffer = pOut + (start_pixel * ch_out_r) + (section * ch_out_r);
 
-  for (i_out_y = start_pixel; i_out_y < stop_pixel; i_out_y++)
+  for (i_out_x = start_pixel; i_out_x < stop_pixel; i_out_x++)
   {
-      if(i_out_y < padding_y_top)
+      if(i_out_x < padding_x_left)
       {
-        for(i_ker_y=i_out_y * stride_y - padding_y_top; i_ker_y < i_out_y * stride_y - padding_y_top + dim_kernel_y * (1 + dil) - dil; i_ker_y += (1 + dil))
+        for(i_ker_x=i_out_x * stride_x - padding_x_left; i_ker_x < i_out_x * stride_x - padding_x_left + dim_kernel_x * (1 + dil) - dil; i_ker_x += (1 + dil))
         {
-            if((i_ker_y < 0) || (i_ker_y >= dim_in_y))
+            if((i_ker_x < 0) || (i_ker_x >= dim_in_x))
             {
               xpulp_nn_zero_mem_u2(pIm2Col, ch_in);
             }
             else
             {
-              xpulp_nn_im2col_u2_to_u2((uint8_t*) pIn + i_ker_y * ch_in_r, pIm2Col, ch_in);
+              xpulp_nn_im2col_u2_to_u2((uint8_t*) pIn + i_ker_x * ch_in_r, pIm2Col, ch_in);
             }
             pIm2Col+=PACK_INT2_SIZE(ch_in);
         }
       }
-      else if(i_out_y < dim_out_y - padding_y_bottom)
+      else if(i_out_x < dim_out_x - padding_x_right)
       {
-          for(i_ker_y=i_out_y * stride_y - padding_y_top; i_ker_y < i_out_y * stride_y - padding_y_top + dim_kernel_y * (1 + dil) - dil; i_ker_y += (1 + dil))
+          for(i_ker_x=i_out_x * stride_x - padding_x_left; i_ker_x < i_out_x * stride_x - padding_x_left + dim_kernel_x * (1 + dil) - dil; i_ker_x += (1 + dil))
           {
-            xpulp_nn_im2col_u2_to_u2((uint8_t*) pIn + i_ker_y * ch_in_r, pIm2Col, ch_in);
+            xpulp_nn_im2col_u2_to_u2((uint8_t*) pIn + i_ker_x * ch_in_r, pIm2Col, ch_in);
             pIm2Col+=PACK_INT2_SIZE(ch_in);
           }
       }
       else
       {
-        for(i_ker_y=i_out_y * stride_y - padding_y_top; i_ker_y < i_out_y * stride_y - padding_y_top + dim_kernel_y * (1 + dil) - dil; i_ker_y += (1 + dil))
+        for(i_ker_x=i_out_x * stride_x - padding_x_left; i_ker_x < i_out_x * stride_x - padding_x_left + dim_kernel_x * (1 + dil) - dil; i_ker_x += (1 + dil))
         {
-            if(i_ker_y < 0 || (i_ker_y >= dim_in_y))
+            if(i_ker_x < 0 || (i_ker_x >= dim_in_x))
             {
               xpulp_nn_zero_mem_u2(pIm2Col, ch_in);
             }
             else
             {
-              xpulp_nn_im2col_u2_to_u2((uint8_t *) pIn + i_ker_y * ch_in_r, pIm2Col, ch_in);
+              xpulp_nn_im2col_u2_to_u2((uint8_t *) pIn + i_ker_x * ch_in_r, pIm2Col, ch_in);
             }
             pIm2Col+=PACK_INT2_SIZE(ch_in);
         }
       }
-      if(pIm2Col == (pIm2ColBase + ((PACK_INT2_SIZE(ch_in) * dim_kernel_y) << 1)))
+      if(pIm2Col == (pIm2ColBase + ((PACK_INT2_SIZE(ch_in) * dim_kernel_x) << 1)))
       {
         pOutBuffer = xpulp_nn_matmul_u2_u2_i2(
           pIm2ColBase,
@@ -124,7 +124,7 @@ void __attribute__((noinline)) xpulp_nn_conv1d_u2_u2_i2(
           pLambda,
           out_mult,
           out_shift,
-          (ch_in * dim_kernel_y),
+          (ch_in * dim_kernel_x),
           ch_out,
           flag_relu,
           flag_batch_norm
@@ -149,8 +149,8 @@ void __attribute__((noinline)) xpulp_nn_conv1d_u2_u2_i2(
       int32_t * k1 = pKappa;
       int32_t * lambda1 = pLambda;
       uint8_t out[4];
-      uint16_t num_col_im2col = ch_in * dim_kernel_y;
-      uint16_t num_col_im2col_w = PACK_INT2_SIZE(ch_in) * dim_kernel_y;
+      uint16_t num_col_im2col = ch_in * dim_kernel_x;
+      uint16_t num_col_im2col_w = PACK_INT2_SIZE(ch_in) * dim_kernel_x;
 
       for(i = 0; i < ch_out; i++)
       {
